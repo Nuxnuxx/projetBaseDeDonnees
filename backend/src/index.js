4d882b5d4244891c8dbde96039d4e075578a2830
ES0TERIX <thomas.carre74@gmail.com>
2023-11-07 18:42:31 +0100

    test POST

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,21 @@
 import express from 'express'
 const app = express()
+const port = 3000
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.use(express.json()) 
+app.post('api/products', (req, res) => {
+	const newProduct = {
+		name: req.body.name, 
+		price: req.body.price
+	}
+	products.push(newProduct)
+	res.status(201).json(newProduct)
+})
