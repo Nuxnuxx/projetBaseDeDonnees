7b89405317aa00115387cbe13f9bf2172b78102b
Nuxnuxx <nuxnux02290@gmail.com>
2023-11-15 16:23:49 +0100

    feat: beautify log and update nav UI

--- frontend/src/components/Nav.svelte
+++ frontend/src/components/Nav.svelte
@@ -2,3 +2,20 @@
 	<a href="/">HOME</a>
 	<a href="/log">LOG</a>
 </nav>
+
+<style>
+	nav {
+		width: 100%;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		height: 4rem;
+		border-bottom: 3px solid green;
+	}
+
+	nav a {
+		text-decoration: none;
+		color: black;
+		font-size: 2rem;
+	}
+</style>
